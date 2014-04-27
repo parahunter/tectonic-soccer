cbf784739e27c4f6ecf1d5c2c8bbbb1a888aee82
parahunter <parahunter@gmail.com>
2014-04-27 03:25:39 +0100

    added nice graphics

---
+++ Assets/Camera/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Ball ball;
+	
+	void Update()
+	{
+		transform.LookAt( ball.transform.position );
+	}
+}
