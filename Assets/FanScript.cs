723bfdf1754eb6acd9a1545dd163ac4d8c81bfcd
dan <d.church@outlook.com>
2014-04-27 16:24:43 +0100

    fan prefab and color script  and seating (lol)

---
+++ Assets/FanScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Color newColor = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));
+		this.transform.Find ("Body").renderer.material.color = newColor;
+		this.transform.Find ("Arms").renderer.material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
