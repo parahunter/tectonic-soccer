b8b2b0ba0c8d345ddabf08c765ed17a1de88a209
dan <d.church@outlook.com>
2014-04-27 15:01:50 +0100

    Added scroreboard  + textures and reset script

---
+++ Assets/ResetScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResetScript : MonoBehaviour {
+
+	Vector3 position;
+
+	Vector3 velocity;
+
+	Quaternion rotation;
+
+	void Reset()
+	{
+		rigidbody.velocity = velocity;
+		transform.rotation = rotation;
+		transform.position = position;
+	}
+
+	// Use this for initialization
+	void Start () {
+		position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		velocity = new Vector3 (rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z);
+		rotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		
+		GameController.instance.onReset += Reset;
+	}
+}
