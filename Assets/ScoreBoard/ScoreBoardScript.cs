73af69d5334e67b428047e6f3f2195565a237264
dan <d.church@outlook.com>
2014-04-27 15:15:13 +0100

    prefab update

---
+++ Assets/ScoreBoard/ScoreBoardScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreBoardScript : MonoBehaviour {
+
+	public Texture2D[] goalTextures;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
