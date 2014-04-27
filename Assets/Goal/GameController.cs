ded79ff41bb5b1486d5ed8cc4b7f77db1f4c117e
dan <d.church@outlook.com>
2014-04-27 03:09:38 +0100

    Added some goal logic event
    and sound controller, needs the sounds tho

--- Assets/Goal/GameController.cs
+++ Assets/Goal/GameController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class GameController : MonoBehaviour 
 {
-	public int player0Goals = 0;
-	public int player1Goals = 0;
+	public int[] playerGoals = new int[2];
+	public int lastScorer = -1;
+	public int winningPlayer = -1;
+	public event System.Action onScored;
 		
 	public static GameController instance
 	{
@@ -20,9 +23,14 @@ public class GameController : MonoBehaviour
 	
 	public void OnGoal(int playerGoal)
 	{
-		
-		
+		++playerGoals[playerGoal];
+		if (playerGoals [0] > playerGoals [1])
+			winningPlayer = 0;
+		else if (playerGoals [0] < playerGoals [1])
+			winningPlayer = 1;
+		else
+			winningPlayer = -1;
+
+		onScored();
 	}
-	
-	
 }
